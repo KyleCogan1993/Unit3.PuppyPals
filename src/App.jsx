2ddc3f11f616b90a36e6b1aeee4a25830ad30681
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-13 14:39:40 -0500

    second page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { puppyList } from './data';
+import {useState} from 'react';
 
 function App() {
-
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   return (
     <>
-      <div>
+      <div className = "App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>})
+        }
       </div>
     </>
   );
